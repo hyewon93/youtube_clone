663ed59d971017745406291bed7b4550c94e712d
hyewon93 <baehw93@gmail.com>
2024-02-08 11:50:51 -0800

    Set right box to show videos for each categories

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -11,7 +11,14 @@ const Feed = () => {
         <Typography className="copyRight" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2024 HW Dev
         </Typography>
+      </Box>
 
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          New <span style={{ color: '#F31503'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   )
